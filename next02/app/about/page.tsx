5639af929bd58cdb05399893da1cbe17040405ae
david-t3 <hidavidt777@gmail.com>
2024-04-04 07:43:28 +0200

    feat: file structure

---
+++ next02/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function About() {
+  throw new Error('Not Today')
+  return (
+    <>
+        <h1>About Page</h1>
+        <Link href='/'>Link to Home Page</Link>
+    </>
+  )
+}
